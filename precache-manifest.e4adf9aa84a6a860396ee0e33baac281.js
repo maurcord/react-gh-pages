897e629178bee0c2616b30eaa2f6d8f1e07c847e
maurcord <67590886+maurcord@users.noreply.github.com>
2020-08-27 14:06:32 -0400

    Updates

---
+++ precache-manifest.e4adf9aa84a6a860396ee0e33baac281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921c9c8c84f56b8701d8c3f81b0da823",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "10746483713be37baff8",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ea5cfc3202a04704897a",
+    "url": "/react-gh-pages/static/js/2.a51ad747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.a51ad747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10746483713be37baff8",
+    "url": "/react-gh-pages/static/js/main.e5655dd8.chunk.js"
+  },
+  {
+    "revision": "66e191b5fd3a61b4dd4a",
+    "url": "/react-gh-pages/static/js/runtime-main.732442c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
